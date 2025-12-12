c1960fb518aecc5bbccf658a2e91e13c0470481c
Dikshanta Chapagain <3108dikshanta@gmail.com>
2025-12-13 00:11:13 +0545

    mcp-server optimized

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def get_multiplication(a: float, b: float):
     result = a * b
     return {"a": a, "b": b, "result": result}
 
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
